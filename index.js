d2c130141455078e60e35f8a0661fb7279b51d8f
LittleLebowski <o.f.atas@gmail.com>
2022-11-23 21:51:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.bottom = "0px";
+dodger.style.left = "180px";
+
+const movment = (e) => {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft(e);
+  }
+  if (e.key === "ArrowRight") {
+    moveDodgerRight(e);
+  }
+};
+
+const moveDodgerLeft = (e) => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  dodger.style.left = `${left - 1}px`;
+};
+
+const moveDodgerRight = (e) => {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+};
+document.addEventListener("keydown", movment);
